51371dec76ba265cefe29137ab96e1bf478fbdfc
Lorenzo Ferrara <61883846+lorenzoferrara@users.noreply.github.com>
2023-04-29 16:34:37 +0200

    Plots

--- datanalysis_results.R
+++ datanalysis_results.R
@@ -40,34 +40,75 @@ prod = prod[prod$FUEL=="E1"]
 
 temp = prod$TECHNOLOGY
 prod$TECH=temp
-for(i in 1:length(temp)){
-  prod$TECH[i] = substr(temp[i], start=1, stop=2)
+
+for(i in unique(temp)){
+  
+  prod$TECH[prod$TECH==i] = substr(i, start=1, stop=2)
   print(i)
 }
 
-prod2=matrix(0,(60-15+1)*length(unique(prod$TECH)),3)
+prod2=matrix(0,(60-15+1)*length(unique(prod$TECH))*length(unique(prod$scen)),4)
 k=1
-for(i in 2015:2060){
-  for(j in unique(prod$TECH)){          
-    prod2[k,1] = i
-    prod2[k,2] = j
-    prod2[k,3] = sum(prod[prod$YEAR==i & prod$TECH==j,5])
-    k=k+1
+for(year in 2015:2060){
+  for(technology in unique(prod$TECH)){
+    for(scenario in unique(prod$scen)){
+      prod2[k,1] = year
+      prod2[k,2] = technology
+      prod2[k,3] = sum(prod[prod$YEAR==year & prod$TECH==technology & prod$scen==scenario,5])
+      prod2[k,4] = scenario
+      k=k+1
+    }
   }
 }
 prod2=as.data.frame(prod2)
-colnames(prod2)=c('YEAR', 'TECH', 'value')
-prod2$value = as.numeric(round(prod2$value), 2)
+colnames(prod2)=c('YEAR', 'TECH', 'value', 'scen')
+prod2$value = round(as.numeric(prod2$value),2)
 {
   x11()
   ggplot(prod2[prod2$value!=0,]) +
   # ggplot(prod2) +
     geom_area(aes(x=as.numeric(YEAR),y=value,fill=TECH)) +
-    # facet_wrap(scen~.,) + 
+    facet_wrap(scen~.,) +
     xlab("year") + ylab("POWER [GW]") + theme_pubr() 
 }
 
 ################################################################################################
 #ACCUMULATED CAPACITY
 
+
+cap <- batch_extract("ACCUMULATEDNEWCAPACITY",all_gdx)[[1]] |> setDT() |> osemosys_sanitize_2()
+cap = cap[cap$value!=99999]
+
+temp = cap$TECHNOLOGY
+cap$TECH=temp
+
+for(i in unique(temp)){
+  cap$TECH[cap$TECH==i] = substr(i, start=1, stop=2)
+  print(i)
+}
+
+cap2=matrix(0,(60-15+1)*length(unique(cap$TECH))*length(unique(cap$scen)),4)
+k=1
+for(year in 2015:2060){
+  for(technology in unique(cap$TECH)){
+    for(scenario in unique(cap$scen)){
+      cap2[k,1] = year
+      cap2[k,2] = technology
+      cap2[k,3] = sum(cap[cap$YEAR==year & cap$TECH==technology & cap$scen==scenario,]$value)
+      cap2[k,4] = scenario
+      k=k+1
+    }
+  }
+}
+cap2=as.data.frame(cap2)
+colnames(cap2)=c('YEAR', 'TECH', 'value', 'scen')
+cap2$value = round(as.numeric(cap2$value),2)
+{
+  x11()
+  ggplot(cap2[cap2$value!=0,]) +
+    # ggplot(cap2) +
+    geom_area(aes(x=as.numeric(YEAR),y=value,fill=TECH)) +
+    facet_wrap(scen~.,) +
+    xlab("year") + ylab("POWER [GW]") + theme_pubr() 
+}
 
