bf2229e40b3e6227311c082d899ec6d39c2ff2d1
Matteo9999 <106628271+Matteo9999@users.noreply.github.com>
2023-05-14 12:13:16 +0200

    Automatizator

--- Italy/run_gams.bat
+++ Italy/run_gams.bat
@@ -1,3 +1,20 @@
-gams osemosys.gms --drought=10
+gams osemosys.gms	 --WaterDemand=	0	 --RCP=	26	 --noatom=	1
+gams osemosys.gms	 --WaterDemand=	10	 --RCP=	26	 --noatom=	1
+gams osemosys.gms	 --WaterDemand=	100	 --RCP=	26	 --noatom=	1
+gams osemosys.gms	 --WaterDemand=	0	 --RCP=	45	 --noatom=	1
+gams osemosys.gms	 --WaterDemand=	10	 --RCP=	45	 --noatom=	1
+gams osemosys.gms	 --WaterDemand=	100	 --RCP=	45	 --noatom=	1
+gams osemosys.gms	 --WaterDemand=	0	 --RCP=	85	 --noatom=	1
+gams osemosys.gms	 --WaterDemand=	10	 --RCP=	85	 --noatom=	1
+gams osemosys.gms	 --WaterDemand=	100	 --RCP=	85	 --noatom=	1
+gams osemosys.gms	 --WaterDemand=	0	 --RCP=	26		
+gams osemosys.gms	 --WaterDemand=	10	 --RCP=	26		
+gams osemosys.gms	 --WaterDemand=	100	 --RCP=	26		
+gams osemosys.gms	 --WaterDemand=	0	 --RCP=	45		
+gams osemosys.gms	 --WaterDemand=	10	 --RCP=	45		
+gams osemosys.gms	 --WaterDemand=	100	 --RCP=	45		
+gams osemosys.gms	 --WaterDemand=	0	 --RCP=	85		
+gams osemosys.gms	 --WaterDemand=	10	 --RCP=	85		
+gams osemosys.gms	 --WaterDemand=	100	 --RCP=	85		
 
 pause
